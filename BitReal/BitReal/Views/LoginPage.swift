b38a63aeedef88d402d76b897022280d07020749
pinru277 <beckychen0521@utexas.edu>
2023-02-27 16:03:20 -0600

    login page UI

--- BitReal/BitReal/Views/LoginPage.swift
+++ BitReal/BitReal/Views/LoginPage.swift
@@ -8,8 +8,45 @@
 import SwiftUI
 
 struct LoginPage: View {
+    @State private var userName = ""
+    @State private var password = ""
+    @State private var loginSuccess = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("Log in")
+                .padding(.top, 50)
+                .font(.system(size: 40))
+                .fontWeight(.bold)
+                .offset(x: -100, y: -200)
+            
+            TextField("Username", text: $userName)
+                .padding()
+                .frame(width: 300)
+                .background(Color.white.opacity(0.1))
+                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.black))
+                .offset(y: -220)
+                .padding(.top, 30)
+            
+            SecureField("Password", text: $password)
+                .padding()
+                .frame(width: 300)
+                .background(Color.white.opacity(0.1))
+                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.black))
+                .offset(y: -220)
+                .padding(.top, 20)
+            
+            Button(action: {if userName == "Longhorn" && password == "longhorn12345"{
+                loginSuccess = true
+            }}){
+                Text("Log in")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 50)
+                    .background(Color("Purple"))
+                    .offset(y: -200)
+            }
+        }
     }
 }
 
--- BitReal/BitReal/Views/SplashView.swift
+++ BitReal/BitReal/Views/SplashView.swift
@@ -9,41 +9,43 @@ import SwiftUI
 
 struct SplashView: View {
     var body: some View {
-        ZStack{
-            Image("splashImage")
-                .resizable()
-                .scaledToFill()
-                .ignoresSafeArea()
-            
-            VStack{
-                Text("BitReal")
-                    .font(.system(size: 55))
-                    .offset(x:50, y:30)
-                
-                Image("logoImage")
+        NavigationView{
+            ZStack{
+                Image("splashImage")
                     .resizable()
-                    .frame(width: 60, height: 60)
-                    .offset(x: -90, y: -70)
+                    .scaledToFill()
+                    .ignoresSafeArea()
                 
-                NavigationLink(destination: LoginPage()) {
-                    Text("LOG IN")
-                        .frame(width: 160, height: 50)
-                        .foregroundColor(.purple)
-                        .background(Color.white)
-                        .cornerRadius(10)
-                        .offset(CGSize(width: -88, height: 268))
+                VStack{
+                    Text("BitReal")
+                        .font(.system(size: 55))
+                        .offset(x:50, y:30)
                     
+                    Image("logoImage")
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                        .offset(x: -90, y: -70)
+                    
+                    NavigationLink(destination: LoginPage()) {
+                        Text("LOG IN")
+                            .frame(width: 160, height: 50)
+                            .foregroundColor(.purple)
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .offset(CGSize(width: -88, height: 268))
+                        
+                    }
+                    NavigationLink(destination: RegisterIView()) {
+                        Text("REGISTER")
+                            .frame(width: 160, height: 50)
+                            .foregroundColor(.white)
+                            .background(Color.purple)
+                            .cornerRadius(10)
+                            .offset(CGSize(width: 88, height: 210))
+                    }
                 }
-                NavigationLink(destination: RegisterIView()) {
-                    Text("REGISTER")
-                        .frame(width: 160, height: 50)
-                        .foregroundColor(.white)
-                        .background(Color.purple)
-                        .cornerRadius(10)
-                        .offset(CGSize(width: 88, height: 210))
-                }
-                
             }
+            .navigationBarHidden(true)
         }
     }
 }
