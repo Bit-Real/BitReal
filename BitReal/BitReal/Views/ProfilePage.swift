fdb3b806aad3d204be37091ee37d642a633f482c
Emmanuel Ihim Jr <emmanuel@Emmanuels-MBP.lan>
2023-03-03 17:58:41 -0600

    First version of profile page, just the UI

--- BitReal/BitReal/Views/ProfilePage.swift
+++ BitReal/BitReal/Views/ProfilePage.swift
@@ -8,10 +8,40 @@
 import SwiftUI
 
 struct ProfilePage: View {
+    let settingsOptions = ["Change Password", "Notifications", "Themes"]
+    
     var body: some View {
         NavigationView {
             ZStack {
-                Color.yellow
+                VStack {
+                    Image("dogImage")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 150, height: 150)
+                        .clipShape(Circle())
+                        .padding()
+                    
+                    Text("Mr. Woofington")
+                        .bold()
+                    
+                    Divider()
+                        .frame(height: 4)
+                        .frame(width: 80)
+                        .overlay(Color("Purple"))
+                        .padding(.bottom, 25)
+                    
+                    List(settingsOptions, id: \.self) { setting in
+                        HStack {
+                            Text(setting)
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                        }
+                        .listRowSeparator(.hidden)
+                    } .listStyle(.plain)
+                    
+                    
+                    
+                }
             }
             .navigationTitle("Profile")
         }
