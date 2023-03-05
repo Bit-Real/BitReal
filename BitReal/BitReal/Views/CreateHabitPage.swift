7570a38b46059168ee9c62ced3e14cac50b6290d
Ali Al-Adhami <aliraad@utexas.edu>
2023-03-04 23:25:44 -0600

    fixed duplicated NavigationView in CreateHabitPage

--- BitReal/BitReal/Views/CreateHabitPage.swift
+++ BitReal/BitReal/Views/CreateHabitPage.swift
@@ -16,63 +16,61 @@ struct CreateHabitPage: View {
     @State private var isOn = false
     
     var body: some View {
-        NavigationView {
-            VStack () {
-                TextField("Habit Name", text: $habitName)
-                    .padding()
-                    .frame(width: 350)
-                    .background(Color.white.opacity(01))
-                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.black))
-                    .padding(.top, 25)
-                
-                TextField("Description", text: $description)
-                    .padding()
-                    .frame(width: 350)
-                    .background(Color.white.opacity(0.1))
-                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.black))
-                    .padding(.top, 20)
-                
-                TextField("Frequency", text: $freq)
-                    .padding()
-                    .frame(width: 350)
-                    .background(Color.white.opacity(0.1))
-                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.black))
-                    .padding(.top, 20)
-                
-                TextField("Alarm", text: $alarm)
-                    .padding()
-                    .frame(width: 350)
-                    .background(Color.white.opacity(0.1))
-                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.black))
-                    .padding(.top, 20)
-                
-                HStack {
-                    Toggle("Private to friends", isOn: $isOn)
-                        .toggleStyle(SwitchToggleStyle(tint: .purple))
-                        .padding(.leading, 17)
-                        .padding(.trailing, 20)
-                }
+        VStack () {
+            TextField("Habit Name", text: $habitName)
                 .padding()
-                
-                Button(action: {}) {
-                    HStack {
-                        Image(systemName: "plus")
-                            .foregroundColor(Color.purple)
-                        Text("Create Habit")
-                    }
-                    .frame(width: 125, height: 5)
-                    .foregroundColor(Color.purple)
-                    .padding()
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 5)
-                            .stroke(.purple, lineWidth: 2)
-                    )
-                }
+                .frame(width: 350)
+                .background(Color.white.opacity(01))
+                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.black))
+                .padding(.top, 25)
+            
+            TextField("Description", text: $description)
                 .padding()
-                Spacer()
+                .frame(width: 350)
+                .background(Color.white.opacity(0.1))
+                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.black))
+                .padding(.top, 20)
+            
+            TextField("Frequency", text: $freq)
+                .padding()
+                .frame(width: 350)
+                .background(Color.white.opacity(0.1))
+                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.black))
+                .padding(.top, 20)
+            
+            TextField("Alarm", text: $alarm)
+                .padding()
+                .frame(width: 350)
+                .background(Color.white.opacity(0.1))
+                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.black))
+                .padding(.top, 20)
+            
+            HStack {
+                Toggle("Private to friends", isOn: $isOn)
+                    .toggleStyle(SwitchToggleStyle(tint: .purple))
+                    .padding(.leading, 17)
+                    .padding(.trailing, 20)
             }
-            .navigationTitle("New Habit")
+            .padding()
+            
+            Button(action: {}) {
+                HStack {
+                    Image(systemName: "plus")
+                        .foregroundColor(Color.purple)
+                    Text("Create Habit")
+                }
+                .frame(width: 125, height: 5)
+                .foregroundColor(Color.purple)
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(.purple, lineWidth: 2)
+                )
+            }
+            .padding()
+            Spacer()
         }
+        .navigationTitle("New Habit")
     }
 }
 
