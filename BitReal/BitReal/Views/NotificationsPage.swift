1b6c122338997d4306e0dc468ca538fe4d7b43c7
Emmanuel Ihim Jr <emmanuelihim7@gmail.com>
2023-04-17 08:37:26 -0500

    Updated Notifications Page with backend data from Firebase

--- BitReal/BitReal/Views/NotificationsPage.swift
+++ BitReal/BitReal/Views/NotificationsPage.swift
@@ -6,75 +6,13 @@
 //
 
 import SwiftUI
-
-struct Friend: Identifiable {
-    let id = UUID()
-    let name: String
-    let username: String
-    let profileImageName: String
-    let comment: String
-}
-
-struct TestUser: Identifiable {
-    let id = UUID()
-    let post: String
-}
-
-struct FriendRequestRow: View {
-    let friend: Friend
-    
-    var body: some View {
-            HStack {
-                Image(friend.profileImageName)
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 100, height: 100)
-                    .cornerRadius(50)
-                    .clipped()
-                
-                VStack (alignment: .leading){
-                    Text(friend.name)
-                        .bold()
-                        .font(.system(size: 20))
-                        
-                    Text(friend.username)
-                        .font(.system(size: 15))
-                        .foregroundColor(.gray)
-                    
-                    HStack {
-                        Button(action: {
-                            print("Confirm Button Pressed")
-                        }, label: {
-                            Text("Confirm")
-                                .foregroundColor(.white)
-                                .padding(.horizontal, 30)
-                                .padding(.vertical, 10)
-                                .background(Color.blue)
-                                .cornerRadius(5)
-                                .bold()
-                        }) .buttonStyle(.borderless)
-                        
-                        Button(action: {
-                            print("Delete Button Pressed")
-                        }, label: {
-                            Text("Delete")
-                                .foregroundColor(.black)
-                                .padding(.horizontal, 30)
-                                .padding(.vertical, 10)
-                                .background(Color.gray.opacity(0.3))
-                                .cornerRadius(5)
-                                .bold()
-                        })
-                    } .buttonStyle(.borderless)
-                    
-                }
-            }
-            .padding(.vertical, 10)
-    }
-}
+import Firebase
+import FirebaseAuth
+import FirebaseFirestore
+import Kingfisher
 
 struct FollowerNotificationsCard: View {
-    let friend: Friend
+    let notification: NotificationModel
     
     var body: some View {
         ZStack (alignment: .leading) {
@@ -82,14 +20,14 @@ struct FollowerNotificationsCard: View {
                 .fill(Color.white)
                 .shadow(radius: 5)
             HStack {
-                Image(friend.profileImageName)
+                KFImage(URL(string: notification.friendProfilePic))
                     .resizable()
                     .aspectRatio(contentMode: .fill)
                     .frame(width: 50, height: 50)
                     .cornerRadius(25)
                     .clipped()
                 
-                Text("\(friend.username) is following you")
+                Text("\(notification.friendUserName) is following you")
                     .font(.system(size: 16))
                     .lineLimit(1)
                     .minimumScaleFactor(0.9)
@@ -115,8 +53,17 @@ struct FollowerNotificationsCard: View {
 }
 
 struct LikeNotificationsCard: View {
-    let friend: Friend
-    let user: TestUser
+    let notification: NotificationModel
+    let postID: String
+    let ref: DocumentReference
+    
+    @State private var caption: String = ""
+    
+    init(notification: NotificationModel) {
+        self.notification = notification
+        self.postID = notification.postID
+        self.ref = Firestore.firestore().collection("posts").document(postID)
+    }
     
     var body: some View {
         ZStack (alignment: .leading) {
@@ -126,28 +73,48 @@ struct LikeNotificationsCard: View {
             
             VStack (alignment: .leading) {
                 HStack {
-                    Image(friend.profileImageName)
+                    KFImage(URL(string: notification.friendProfilePic))
                         .resizable()
                         .aspectRatio(contentMode: .fill)
                         .frame(width: 50, height: 50)
                         .cornerRadius(25)
                         .clipped()
                     
-                    Text("\(friend.username) liked your post")
+                    Text("\(notification.friendUserName) liked your post")
                         .font(.system(size: 16))
                 }
                 
-                Text("\(user.post)")
+                Text(caption)
                     .lineLimit(3)
                     .foregroundColor(Color(red: 0.5, green: 0.5, blue: 0.5))
-            } .padding()
+            }
+            .padding()
+            .onAppear {
+                fetchCaption()
+            }
         }
     }
+    
+    func fetchCaption() {
+            ref.getDocument { (document, error) in
+                if let error = error {
+                    print("Error fetching post caption: \(error.localizedDescription)")
+                } else if let document = document, document.exists {
+                    let data = document.data()
+                    if let caption = data?["caption"] as? String {
+                        self.caption = caption
+                    } else {
+                        print("Caption not found in document")
+                    }
+                } else {
+                    print("Post document not found")
+                }
+            }
+        }
 }
 
 struct CommentNotificationsCard: View {
-    let friend: Friend
-    let user: TestUser
+    let notification: NotificationModel
     
     var body: some View {
         ZStack (alignment: .leading) {
@@ -157,18 +124,18 @@ struct CommentNotificationsCard: View {
             
             VStack (alignment: .leading) {
                 HStack {
-                    Image(friend.profileImageName)
+                    KFImage(URL(string: notification.friendProfilePic))
                         .resizable()
                         .aspectRatio(contentMode: .fill)
                         .frame(width: 50, height: 50)
                         .cornerRadius(25)
                         .clipped()
                     
-                    Text("\(friend.username) commented on your post")
+                    Text("\(notification.friendUserName) commented on your post")
                         .font(.system(size: 16))
                 }
                 
-                Text("\(friend.comment)")
+                Text(notification.comment)
                     .lineLimit(2)
                     .foregroundColor(Color(red: 0.5, green: 0.5, blue: 0.5))
             } .padding()
@@ -176,19 +143,20 @@ struct CommentNotificationsCard: View {
     }
 }
 
-
 struct NotificationsPage: View {
-    let friends: [Friend] = [
-        Friend(name: "John Doe", username: "@Johnny", profileImageName: "pic0", comment: "Great job completing your habit today!"),
-        Friend(name: "Jane Doe", username: "@JD", profileImageName: "pic1", comment: "Wow, I can't believe how JACKED you look now. Congrats on the 6-pack Dad :)"),
-        Friend(name: "Bob Smith", username: "@Bobby", profileImageName: "pic2", comment: "Keep killing it 🔥")
-    ]
-    
-    let users: [TestUser] = [
-        TestUser(post: "Up branch to easily missed by do. Admiration considered acceptance too led one melancholy expression. Are will took form the nor true. Winding enjoyed minuter her letters evident use eat colonel. He attacks observe mr cottage inquiry am examine gravity. Are dear but near left was. Year kept on over so as this of. She steepest doubtful betrayed formerly him. Active one called uneasy our seeing see cousin tastes its. Ye am it formed indeed agreed relied piqued."),
-        TestUser(post: "Improve ashamed married expense bed her comfort pursuit mrs. Four time took ye your as fail lady."),
-        TestUser(post: "test post 3"),
-    ]
+    @State var notifications: [NotificationModel] = []
+    let db = Firestore.firestore()
+    let ref = Firestore.firestore().collection("notifications")
+    let user = Auth.auth().currentUser
+    let userID: String?
+        
+    init() {
+        if let user = Auth.auth().currentUser {
+            self.userID = user.uid
+        } else {
+            self.userID = nil
+        }
+    }
     
     var body: some View {
         VStack(alignment: .leading) {
@@ -196,17 +164,43 @@ struct NotificationsPage: View {
                 .font(.system(size: 36, weight: .bold))
                 .padding()
             
-            List(Array(zip(friends, users)), id: \.0.id) { (friend, user) in
-                FollowerNotificationsCard(friend: friend)
-                    .listRowSeparator(.hidden)
-                LikeNotificationsCard(friend: friend, user: user)
-                    .listRowSeparator(.hidden)
-                CommentNotificationsCard(friend: friend, user: user)
-                    .listRowSeparator(.hidden)
+            List(notifications, id: \.id) { notification in
+                switch notification.type {
+                    case "Like":
+                        LikeNotificationsCard(notification: notification)
+                            .listRowSeparator(.hidden)
+                    case "Comment":
+                        CommentNotificationsCard(notification: notification)
+                            .listRowSeparator(.hidden)
+                    case "Follow":
+                        FollowerNotificationsCard(notification: notification)
+                            .listRowSeparator(.hidden)
+                    default:
+                        EmptyView()
+                    }
             }
             .listStyle(PlainListStyle())
+            .onAppear() {
+                fetchNotifications()
+            }
         }
-        
+    }
+    
+    func fetchNotifications() {
+        guard let userID = userID else {
+            return
+        }
+        ref.whereField("userID", isEqualTo: userID).getDocuments { (snapshot, error) in
+            if let error = error {
+                print("Error fetching notifications: \(error.localizedDescription)")
+                return
+            }
+            if let snapshot = snapshot {
+                self.notifications = snapshot.documents.compactMap { document in
+                    try? document.data(as: NotificationModel.self)
+                }
+            }
+        }
     }
 }
 
